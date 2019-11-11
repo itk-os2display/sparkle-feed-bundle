0fc633447ddd27f8c5e47b47458194d5c1e2feea
Troels Ugilt Jensen <tujensen@gmail.com>
2019-11-11 13:27:40 +0100

    DOKKWAY-298: Minified js

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+var gulp = require('gulp');
+
+// Plugins.
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+var rename = require('gulp-rename');
+var yaml = require('js-yaml');
+var fs = require('fs');
+var header = require('gulp-header');
+
+var adminBuildDir = 'Resources/public/assets/build';
+
+// Get information for top of minified files.
+var pkg = require('./version.json');
+var banner = [
+  '/**',
+  ' * @name <%= pkg.name %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.link %>',
+  ' */',
+  ''
+].join('\n');
+
+// We only want to process our own non-processed JavaScript files.
+var adminJsPath = (function () {
+  var configFiles = [
+      'Resources/config/angular.yml'
+    ],
+    jsFiles = [],
+
+    // Breadth-first descend into data to find "files".
+    buildFiles = function (data) {
+      if (typeof(data) === 'object') {
+        for (var p in data) {
+          if (p === 'files') {
+            jsFiles = jsFiles.concat(data[p]);
+          }
+          else {
+            buildFiles(data[p]);
+          }
+        }
+      }
+    };
+
+  configFiles.forEach(function (path) {
+    var data = yaml.safeLoad(fs.readFileSync(path, 'utf8'));
+    buildFiles(data);
+  });
+
+  return jsFiles.map(function (file) {
+    return 'Resources/public/' + file.split('bundles/os2displaysparklefeed/')[1];
+  });
+}());
+
+/**
+ * Run Javascript through JSHint.
+ */
+gulp.task('jshint', function () {
+  return gulp.src(adminJsPath)
+  .pipe(jshint())
+  .pipe(jshint.reporter(stylish));
+});
+
+/**
+ * Build single app.js file.
+ */
+gulp.task('js', function () {
+    return gulp.src(adminJsPath)
+    .pipe(concat('os2displaysparklefeed.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(rename({extname: ".min.js"}))
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(gulp.dest(adminBuildDir));
+  }
+);
+
+/**
+ * Build single app.js file.
+ */
+gulp.task('js-src', function (done) {
+  adminJsPath.forEach(function (path) {
+    process.stdout.write(path + '\n');
+  });
+
+  done();
+});
